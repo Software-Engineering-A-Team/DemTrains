9dfaad546e00d4a98aadeed7144291ea41bf6245
mvbp <mvater.mvbp@live.com>
2015-02-22 21:10:24 -0500

    Initial Commit. Included some basic GUIs

---
+++ CTCOffice/src/ctcOfficeGUI/CTCOfficeApplication.java
@@ -0,0 +1,53 @@
+package ctcOfficeGUI;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JSplitPane;
+import javax.swing.JScrollPane;
+
+public class CTCOfficeApplication {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					CTCOfficeApplication window = new CTCOfficeApplication();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public CTCOfficeApplication() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new CTCOfficeGUIImplementation();
+		frame.setBounds(100, 100, 836, 530);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		
+		
+		
+		
+		
+	}
+
+}
