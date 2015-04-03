9117bd55c83b7d5d6fb1bfa3f0aea25374350118
Kurt Mohler <kurtamohler@gmail.com>
2015-04-03 15:28:26 -0400

    Added SystemGui class

---
+++ TrainSim/src/system_wrapper/SystemGui.java
@@ -0,0 +1,67 @@
+package system_wrapper;
+
+import train_controller.TrainControllerGui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+import javax.swing.JLabel;
+
+import java.awt.Font;
+
+import javax.swing.JToggleButton;
+import javax.swing.JRadioButton;
+import javax.swing.JProgressBar;
+import javax.swing.JSlider;
+import javax.swing.JCheckBox;
+import javax.swing.ButtonGroup;
+import javax.swing.JTabbedPane;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JPanel;
+
+public class SystemGui extends JFrame {
+  public static void main(String[] args) {
+    EventQueue.invokeLater(new Runnable() {
+      public void run() {
+        try {
+          SystemGui window = new SystemGui();
+          window.setVisible(true);
+        } catch (Exception e) {
+          e.printStackTrace();
+        }
+      }
+    });
+  }
+  
+  public SystemGui() {
+    
+    JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+    getContentPane().add(tabbedPane, BorderLayout.CENTER);
+    
+    JPanel panelCtc = new JPanel();
+    tabbedPane.addTab("CTC", null, panelCtc, null);
+    
+    JPanel panelMbo = new JPanel();
+    tabbedPane.addTab("MBO", null, panelMbo, null);
+    
+    JPanel panelTrackModel = new JPanel();
+    tabbedPane.addTab("Track Model", null, panelTrackModel, null);
+    
+    JPanel panelTrackController = new JPanel();
+    tabbedPane.addTab("Track Controller", null, panelTrackController, null);
+    
+    JPanel panelTrainModel = new JPanel();
+    tabbedPane.addTab("Train Model", null, panelTrainModel, null);
+    
+    JFrame frameTrainController = new TrainControllerGui();
+    tabbedPane.addTab("Train Controller", null, frameTrainController.getContentPane(), null);
+  }
+}
