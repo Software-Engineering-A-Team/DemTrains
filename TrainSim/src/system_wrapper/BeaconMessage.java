1a51edfd2b4530c8a37ca33378bc5597bc2b0a14
Kurt Mohler <kurtamohler@gmail.com>
2015-04-03 14:22:42 -0400

    Added BeaconMessage class

---
+++ TrainSim/src/system_wrapper/BeaconMessage.java
@@ -0,0 +1,9 @@
+package system_wrapper;
+
+class BeaconMessage{
+  public String stationName;
+  public float distanceFromStation;
+  public int trainID;
+  boolean rightSide;
+  boolean leftSide;
+}
