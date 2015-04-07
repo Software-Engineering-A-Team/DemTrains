b099849078555da76f1aaa14789c8a8e30d7a721
mvbp <mvater.mvbp@live.com>
2015-04-06 20:33:49 -0400

    Some additional Code

--- TrainSim/src/ctc_office/CTCDriver.java
+++ TrainSim/src/ctc_office/CTCDriver.java
@@ -2,7 +2,7 @@ package ctc_office;
 
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
+import java.util.ArrayList;
 
 public class CTCDriver {
 	HashMap <String, TrackLayout> lines;
@@ -10,6 +10,23 @@ public class CTCDriver {
 	public CTCDriver() {
 		lines = new HashMap<String, TrackLayout>();
 		manuallyRoutedTrains = new HashMap<String, HashSet<String>>();
+	}
+
+	/*
+	 * Sets the trackLayout and the scheduler to fixed block mode
+	 */
+	public boolean disableMBOMode() {
+		
+		
+		return true;
+	}
+
+	/*
+	 * Sets the trackLayout and the scheduler to MBO mode
+	 */
+	public boolean enableMBOMode() {
+		
+		return true;
 	}
 	
 	 /*
@@ -67,7 +84,31 @@ public class CTCDriver {
 		lines.get(lineName).manuallyRouteTrain(newRoute);
 		return true;
 	}
-	
+
+	/*
+	 * Sends the train routes to the Track Controller
+	 * If a route is not approved, it is updated in the CTC
+	 */
+	public void routeTrains(String lineName) {
+		ArrayList<TrainRoute> routes = lines.get(lineName).getUpdatedTrainRoutes();
+		for (TrainRoute r : routes) {
+			// send the route to the track controller
+			// if it is not approved
+				//update the route in the TrackLayout class
+		}
+		
+	}
+
+	/*
+	 * Used by the MBO to set the actual locations of the trains.
+	 * Takes in the total distance each train has traveled and calculates the new location.
+	 * Calculates all of the new distances at once
+	 */
+	public boolean setActualTrainLocations(String lineName, HashMap<String, Double> trainDistanceMap) {
+		lines.get(lineName).setActualTainLocations(trainDistanceMap);
+		return true;
+	}
+
 	/*
 	 * Sets block blockNumber on line lineName as Broken or not true.
 	 * If status is true, the track will be set to broken
@@ -93,10 +134,29 @@ public class CTCDriver {
 	 * If status is true, the track will be set to broken
 	 * If the status is false, the track will be set to not broken
 	 */
-	public void setOccupiedBlocks(String lineName, List<Integer> occupiedBlocks) {
-		for (Integer blockNumber: occupiedBlocks) {
-			lines.get(lineName).setOccupiedBlock(blockNumber);
-		}
+	public void setOccupiedBlock(String lineName, int blockNumber) {
+		
+		lines.get(lineName).setOccupiedBlock(blockNumber);
 	}
 	
+	/*
+	 * Creates a new track layout given a graph of blocks and a list of blockData
+	 */
+	public boolean setTrackLayout(String lineName, DirectedMultigraph layout, ArrayList<track_model.TrackBlock> blockData) {
+		lines.put(lineName, new TrackLayout(layout, blockData));
+	}
+
+	/*
+	 * Toggles the position of a switch
+	 */
+	public void toggleSwitchPosition(String lineName, int blockNumber) {
+		lines.get(lineName).toggleSwitchPosition(blockNumber);
+	}
+
+	/*
+	 * Toggles the state of a railway crossing
+	 */
+	public void toggleRailwayCrossing(String lineName, int blockNumber) {
+		lines.get(lineName).toggleRailwayCrossing(blockNumber);
+	}
 }
--- TrainSim/src/ctc_office/TrackLayout.java
+++ TrainSim/src/ctc_office/TrackLayout.java
@@ -1,5 +1,43 @@
 package ctc_office;
 
-public class TrackLayout {
+import java.util.ArrayList;
+import java.util.HashMap;
 
-}
+import org.jgrapht.graph.DirectedMultigraph;
+
+public class TrackLayout {
+	private final DirectedMultigraph<Integer, DefaultEdge> layout;
+	private final ArrayList<TrackBlock> blockData = new ArrayList<TrackBlock>();
+	private final TrainRouter trainRouter = new TrainRouter();
+
+	public TrackLayout(DirectedMultigraph<Integer, DefaultEdge> tLayout, ArrayList<track_model.TrackBlock> tBlockData) {
+		layout = tLayout;
+		for (track_model.TrackBlock blockData : blockData) {
+			// build the ctc trackBlocks from the actual trackBlocks
+			// TrackBlock b = new TrackBlock();
+			// blockData.add(TrackBlock.blockNumber, b);
+		}
+	}
+
+	/*
+	 * returns a list of beacon strings that need to be updated for trains approaching a station
+	 */
+	public HashMap<Integer, String> getBeaconStrings() {
+		HashMap<Integer, String> beaconStrings = new HashMap<Integer, String>();
+		// for each station
+			// find train that will reach the station next
+			// build the beaconString
+			// add it to the list
+		return beaconStrings;
+	}
+	
+	/*
+	 * Gets the train route for all dispatched trains for this tick
+	 */
+	public ArrayList<TrainRoute> getUpdatedTrainRoutes() {
+		ArrayList<TrainRoute> routes = new ArrayList<TrainRoute>();
+		
+		return routes;
+	}
+ }
+ 
